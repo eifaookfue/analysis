6b06dbe0e1d24238ed4fa4a99b6fef0015c3b16c
s7.nakamura <s7.nakamura@gmail.com>
2019-10-19 16:23:28 +0900

    メソッド内で定義されていた正規表現を外出し

--- log/src/main/scala/jp/co/nri/nefs/tool/log/common/utils/RegexUtils.scala
+++ log/src/main/scala/jp/co/nri/nefs/tool/log/common/utils/RegexUtils.scala
@@ -1,12 +1,12 @@
 package jp.co.nri.nefs.tool.log.common.utils
 
 object RegexUtils {
+  val regex = """(.*)_(OMS_.*)_(.*)_([0-9][0-9][0-9][0-9][0-9][0-9])_([0-9]*).log$""".r
   case class OMSAplInfo(appName: String, env: String, computer: String, userId: String, startTime: String){
     val tradeDate: String = startTime.take(8)
   }
 
   def getOMSAplInfo(fileName: String): Option[OMSAplInfo] = {
-    lazy val regex = """(.*)_(OMS_.*)_(.*)_([0-9][0-9][0-9][0-9][0-9][0-9])_([0-9]*).log$""".r
     fileName match {
       case regex(appName, env, computer, userId, startTime) => Some(OMSAplInfo(appName, env, computer, userId, startTime))
       case _ => None
