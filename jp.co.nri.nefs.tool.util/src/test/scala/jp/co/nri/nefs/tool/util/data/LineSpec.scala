d11676811958d173ea3b740d0dfb28292e16c6af
eifaookfue <s7.nakamura@gmail.com>
2020-09-12 22:50:26 +0900

    Update LineSpec to use Onedrive's Excel test data.

--- jp.co.nri.nefs.tool.util/src/test/scala/jp/co/nri/nefs/tool/util/data/LineSpec.scala
+++ jp.co.nri.nefs.tool.util/src/test/scala/jp/co/nri/nefs/tool/util/data/LineSpec.scala
@@ -5,17 +5,18 @@ import java.nio.file.{Files, Paths}
 import org.scalatest.{BeforeAndAfterAll, FeatureSpec, GivenWhenThen}
 import Lines._
 import com.typesafe.config.ConfigFactory
-import org.apache.poi.ss.usermodel.WorkbookFactory
+import org.apache.poi.ss.usermodel.{Sheet, WorkbookFactory}
 
 class LineSpec extends FeatureSpec with GivenWhenThen with BeforeAndAfterAll {
 
   private val config = ConfigFactory.load()
   private val baseDir = Paths.get(config.getString(LineSpec.BASE_DIR))
   private val bookName = config.getString(LineSpec.BOOK_NAME)
-  private val url = getClass.getClassLoader.getResource(bookName)
-  private val in = Files.newInputStream(Paths.get(url.toURI))
+  //private val url = getClass.getClassLoader.getResource(bookName)
+  private val in = Files.newInputStream(baseDir.resolve(bookName))
   private val inBook = WorkbookFactory.create(in)
-  private var caseName = ""
+  val inSheet: Sheet = inBook.getSheet(LineSpec.SHEET_NAME)
+  private var rownum = 0
 
   override def afterAll(): Unit = {
     in.close()
@@ -26,7 +27,7 @@ class LineSpec extends FeatureSpec with GivenWhenThen with BeforeAndAfterAll {
     scenario("a list method is used only 1 time in mapping") {
 
       Given("list method is placed at the top of the elements in mapping")
-      caseName = "case1"
+      rownum += 3
       val parentLine1 = Line(mapping(
         key(0, 2) -> list(mapping(
           key(0) -> text,
@@ -35,8 +36,8 @@ class LineSpec extends FeatureSpec with GivenWhenThen with BeforeAndAfterAll {
         key(4) -> text,
         key(5) -> number
       )(Parent1.apply)(Parent1.unapply))
-      val inSheet1 = inBook.getSheet(caseName)
-      val row1 = inSheet1.getRow(0)
+
+      val row1 = inSheet.getRow(rownum)
 
       When("a bind and get method is called")
       val parent1 = parentLine1.bind(row1).get
@@ -47,10 +48,10 @@ class LineSpec extends FeatureSpec with GivenWhenThen with BeforeAndAfterAll {
         ===
           Parent1(List(Child1("c1", 1), Child1("c2", 2)), "p1", 3)
       )
-      println(s"Got object at $caseName is $parent1")
+      println(s"Got object at is $parent1")
 
       Given("list method is placed at the middle of the elements in mapping")
-      caseName = "case2"
+      rownum += 2
       val parentLine2 = Line(mapping(
         key(0) -> text,
         key(1, 2) -> list(mapping(
@@ -59,8 +60,8 @@ class LineSpec extends FeatureSpec with GivenWhenThen with BeforeAndAfterAll {
         )(Child2.apply)(Child2.unapply)),
         key(5) -> number
       )(Parent2.apply)(Parent2.unapply))
-      val inSheet2 = inBook.getSheet(caseName)
-      val row2 = inSheet2.getRow(0)
+      println(s"rownum=$rownum")
+      val row2 = inSheet.getRow(rownum)
 
       When("a bind and get method is called")
       val parent2 = parentLine2.bind(row2).get
@@ -71,10 +72,10 @@ class LineSpec extends FeatureSpec with GivenWhenThen with BeforeAndAfterAll {
           ===
           Parent2("p1", List(Child2("c1", 1), Child2("c2", 2)), 3)
       )
-      println(s"Got object at $caseName is $parent2")
+      println(s"Got object is $parent2")
 
       Given("list method is placed at the bottom of the elements in mapping")
-      caseName = "case3"
+      rownum += 2
       val parentLine3 = Line(mapping(
         key(0) -> text,
         key(1) -> number,
@@ -83,8 +84,7 @@ class LineSpec extends FeatureSpec with GivenWhenThen with BeforeAndAfterAll {
           key(1) -> number
         )(Child3.apply)(Child3.unapply))
       )(Parent3.apply)(Parent3.unapply))
-      val inSheet3 = inBook.getSheet(caseName)
-      val row3 = inSheet3.getRow(0)
+      val row3 = inSheet.getRow(rownum)
 
       When("a bind and get method is called")
       val parent3 = parentLine3.bind(row3).get
@@ -95,14 +95,14 @@ class LineSpec extends FeatureSpec with GivenWhenThen with BeforeAndAfterAll {
           ===
           Parent3("p1", 1, List(Child3("c1", 2), Child3("c2", 3)))
       )
-      println(s"Got object at $caseName is $parent3")
+      println(s"Got object is $parent3")
 
     }
 
     scenario("list methods are used 2 times in mapping") {
 
       Given("list method is placed at the top and bottom of the elements in mapping")
-      caseName = "case4"
+      rownum += 3
       val parentLine4 = Line(mapping(
         key(0, 2) -> list(mapping(
           key(0) -> text,
@@ -114,8 +114,7 @@ class LineSpec extends FeatureSpec with GivenWhenThen with BeforeAndAfterAll {
           key(1) -> number
         )(Child4.apply)(Child4.unapply))
       )(Parent4.apply)(Parent4.unapply))
-      val inSheet4 = inBook.getSheet(caseName)
-      val row4 = inSheet4.getRow(0)
+      val row4 = inSheet.getRow(rownum)
 
       When("a bind and get method is called")
       val parent4 = parentLine4.bind(row4).get
@@ -126,14 +125,14 @@ class LineSpec extends FeatureSpec with GivenWhenThen with BeforeAndAfterAll {
           ===
           Parent4(List(Child4("c1", 1), Child4("c2", 2)), "p1", List(Child4("c1", 3), Child4("c2", 4)))
       )
-      println(s"Got object at $caseName is $parent4")
+      println(s"Got object is $parent4")
 
     }
 
     scenario("a list method is used within a list method") {
 
       Given("a list method is placed at the bottom of the elements, and another list method is placed in list method")
-      caseName = "case5"
+      rownum += 3
       val parentLine5 = Line(mapping(
         key(0) -> text,
         key(1) -> number,
@@ -146,8 +145,7 @@ class LineSpec extends FeatureSpec with GivenWhenThen with BeforeAndAfterAll {
           )(GrandChild5.apply)(GrandChild5.unapply))
         )(Child5.apply)(Child5.unapply))
       )(Parent5.apply)(Parent5.unapply))
-      val inSheet5 = inBook.getSheet(caseName)
-      val row5 = inSheet5.getRow(0)
+      val row5 = inSheet.getRow(rownum)
 
       When("a bind and get method is called")
       val parent5 = parentLine5.bind(row5).get
@@ -167,7 +165,7 @@ class LineSpec extends FeatureSpec with GivenWhenThen with BeforeAndAfterAll {
             )
         )
       )
-      println(s"Got object at $caseName is $parent5")
+      println(s"Got object is $parent5")
 
       /*val outPath = Paths.get("C:/tmp/out.xlsx")
       write(parentLine5, Seq(parent), outPath)
@@ -189,7 +187,7 @@ class LineSpec extends FeatureSpec with GivenWhenThen with BeforeAndAfterAll {
     scenario("a list method is used on invalid data.") {
 
       Given("a number is missing on data")
-      caseName = "case6"
+      rownum += 3
       val parentLine6 = Line(mapping(
         key(0) -> text,
         key(1) -> number,
@@ -198,8 +196,7 @@ class LineSpec extends FeatureSpec with GivenWhenThen with BeforeAndAfterAll {
           key(1) -> number
         )(Child6.apply)(Child6.unapply)),
       )(Parent6.apply)(Parent6.unapply))
-      val inSheet6 = inBook.getSheet(caseName)
-      val row6 = inSheet6.getRow(0)
+      val row6 = inSheet.getRow(rownum)
 
       When("a bind and get method is called")
       val parent6 = parentLine6.bind(row6).get
@@ -211,7 +208,7 @@ class LineSpec extends FeatureSpec with GivenWhenThen with BeforeAndAfterAll {
           ===
           Parent6("p1", 1, List(Child6("c2", 3)))
       )
-      println(s"Got object at $caseName is $parent6")
+      println(s"Got object is $parent6")
 
     }
 
@@ -223,13 +220,12 @@ class LineSpec extends FeatureSpec with GivenWhenThen with BeforeAndAfterAll {
     scenario("String is set at the cell where number is expected.") {
 
       Given("String(s2) is set at cell where number is expected.")
-      caseName = "case7"
+      rownum += 4
       val parentLine7 = Line(mapping(
         key(0) -> text,
         key(1) -> number
       )(Parent7.apply)(Parent7.unapply))
-      val inSheet7 = inBook.getSheet(caseName)
-      val row7 = inSheet7.getRow(0)
+      val row7 = inSheet.getRow(rownum)
 
       When("a bind method is called")
       val parendLine7Binded = parentLine7.bind(row7)
@@ -244,19 +240,18 @@ class LineSpec extends FeatureSpec with GivenWhenThen with BeforeAndAfterAll {
 
     scenario("a optional method wraps text and number") {
       Given("existing text and existing number")
-      caseName = "case8"
+      rownum += 4
       val parentLine8 = Line(mapping(
         key(0) -> optional(text),
         key(1) -> optional(number)
       )(ParentOp.apply)(ParentOp.unapply))
-      val inSheet8 = inBook.getSheet(caseName)
-      val row8 = inSheet8.getRow(0)
+      val row8 = inSheet.getRow(rownum)
 
       When("a bind and get method is called")
       val parent8 = parentLine8.bind(row8).get
 
       Then("existing object can be get.")
-      println(s"Got object at $caseName is $parent8")
+      println(s"Got object is $parent8")
       assert(
         parent8
           ===
@@ -264,19 +259,18 @@ class LineSpec extends FeatureSpec with GivenWhenThen with BeforeAndAfterAll {
       )
 
       Given("null and existing number")
-      caseName = "case9"
+      rownum += 2
       val parentLine9 = Line(mapping(
         key(0) -> optional(text),
         key(1) -> optional(number)
       )(Parent9.apply)(Parent9.unapply))
-      val inSheet9 = inBook.getSheet(caseName)
-      val row9 = inSheet9.getRow(0)
+      val row9 = inSheet.getRow(rownum)
 
       When("a bind and get method is called")
       val parent9 = parentLine9.bind(row9).get
 
       Then("existing object can be get.")
-      println(s"Got object at $caseName is $parent9")
+      println(s"Got object is $parent9")
       assert(
         parent9
           ===
@@ -284,19 +278,18 @@ class LineSpec extends FeatureSpec with GivenWhenThen with BeforeAndAfterAll {
       )
 
       Given("existing text and null")
-      caseName = "case10"
+      rownum += 2
       val parentLine10 = Line(mapping(
         key(0) -> optional(text),
         key(1) -> optional(number)
       )(Parent10.apply)(Parent10.unapply))
-      val inSheet10 = inBook.getSheet(caseName)
-      val row10 = inSheet10.getRow(0)
+      val row10 = inSheet.getRow(rownum)
 
       When("a bind and get method is called")
       val parent10 = parentLine10.bind(row10).get
 
       Then("existing object can be get.")
-      println(s"Got object at $caseName is $parent10")
+      println(s"Got object is $parent10")
       assert(
         parent10
           ===
@@ -306,7 +299,7 @@ class LineSpec extends FeatureSpec with GivenWhenThen with BeforeAndAfterAll {
 
     scenario("a optional method wraps list") {
       Given("existing list")
-      caseName = "case11"
+      rownum += 3
       val parentOpListLine = Line(mapping(
         key(0) -> text,
         key(1, 2) -> optional(list(mapping(
@@ -314,14 +307,13 @@ class LineSpec extends FeatureSpec with GivenWhenThen with BeforeAndAfterAll {
           key(1) -> number
         )(ChildOpList.apply)(ChildOpList.unapply)))
       )(ParentOpList.apply)(ParentOpList.unapply))
-      val inSheet11 = inBook.getSheet(caseName)
-      val row11 = inSheet11.getRow(0)
+      val row11 = inSheet.getRow(rownum)
 
       When("a bind and get method is called")
       val parent11 = parentOpListLine.bind(row11).get
 
       Then("existing object can be get.")
-      println(s"Got object at $caseName is $parent11")
+      println(s"Got object is $parent11")
       assert(
         parent11
           ===
@@ -329,15 +321,14 @@ class LineSpec extends FeatureSpec with GivenWhenThen with BeforeAndAfterAll {
       )
 
       Given("null list")
-      caseName = "case12"
-      val inSheet12 = inBook.getSheet(caseName)
-      val row12 = inSheet12.getRow(0)
+      rownum += 3
+      val row12 = inSheet.getRow(rownum)
 
       When("a bind and get method is called")
       val parent12 = parentOpListLine.bind(row12).get
 
       Then("existing object can be get.")
-      println(s"Got object at $caseName is $parent12")
+      println(s"Got object is $parent12")
       assert(
         parent12
           ===
@@ -354,5 +345,6 @@ object LineSpec {
   final val BASE_CONFIG = "LineSpec"
   final val BASE_DIR = BASE_CONFIG + ".base-dir"
   final val BOOK_NAME = BASE_CONFIG + ".book-name"
+  final val SHEET_NAME = "sheet1"
 }
 
